bfe9a0d6c28ea0b708e13a93733a7c631c4a5649
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-05 18:53:54 +0200

    [UPD] Added footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,1 +1,24 @@
-<footer>Footer</footer>
+<footer>
+    <div class="container">
+        <div class="row text-white">
+            <div class="col-6 text-start">
+                <p class="company">Boolando s.r.l</p>
+                <div class="footer-info">
+                    <p>Informazioni legali</p>
+                    <p>Normativa sulla privacy</p>
+                    <p>Diritto di recesso</p>
+                </div>
+            </div>
+            <div class="col-6 text-end">
+                <p>Trovaci anche su</p>
+                <div class="footer-icons">
+                    <i class="fa-brands fa-square-twitter"></i>
+                    <i class="fa-brands fa-square-facebook"></i>
+                    <i class="fa-brands fa-square-instagram"></i>
+                    <i class="fa-brands fa-square-pinterest"></i>
+                    <i class="fa-brands fa-square-youtube"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
